13a6a2c4a5b6add47a558579a0039ac0d4af0cb7
eduallochio <eduallochio2@outlook.com>
2023-11-13 18:55:45 -0300

    feat: add third promo banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -23,7 +23,7 @@ export default async function Home() {
 
 
     return (
-        <div>
+        <div className="flex flex-col gap-8">
             <PromoBanner
                 src="/banner-home-01.png"
                 alt="Até 55% de desconto esse mês!"
@@ -32,7 +32,7 @@ export default async function Home() {
                 <Carigories />
             </div>
 
-            <div className="mt-8">
+            <div>
                 <SectionTitle>Ofertas</SectionTitle>
                 <ProductList products={deals} />
             </div>
@@ -42,10 +42,18 @@ export default async function Home() {
                 alt="Até 55% de desconto em mouses!"
             />
 
-            <div className="mt-8">
+            <div>
                 <SectionTitle>Teclados</SectionTitle>
                 <ProductList products={keyboards} />
             </div>
+
+            <div>
+                <PromoBanner
+                    src="/banner-home-03.png"
+                    alt="Até 55% de desconto em mouses!"
+                />
+            </div>
+
         </div>
     )
 
