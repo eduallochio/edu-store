801c065639e007ebe41ae6b01a1a509e03788dc1
eduallochio.github.io <43136553+eduallochio@users.noreply.github.com>
2023-11-21 19:36:11 -0300

    Feat: Correção de importação e local de arquivos que estavão dando bug no icon de desconto

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -1,14 +1,15 @@
 import { ProductWithTotalPrice } from "@/helpers/product";
-import { ArrowDownIcon, Badge } from "lucide-react";
+import { ArrowDownIcon } from "lucide-react";
 import Image from "next/image";
+import { Badge } from "./badge";
 
 interface ProductItemProps {
 	product: ProductWithTotalPrice
 }
 const ProductItem = ({ product }: ProductItemProps) => {
 	return (
-		<div className="flex flex-col gap-4 max-w-[170px]">
-			<div className="relative flex h-[170px] w-[170px] items-center justify-center rounded-lg bg-accent">
+		<div className="flex max-w-[156px] flex-col gap-4">
+			<div className="relative flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
 				<Image
 					src={product.imageUrls[0]}
 					height={0}
@@ -22,19 +23,20 @@ const ProductItem = ({ product }: ProductItemProps) => {
 				/>
 
 				{product.discountPercentage > 0 && (
-					<Badge className="absolut left-3 top-3 px-2 py=[2px]">
+					<Badge className=" absolute left-3 top-2 px-2 py-[2px]">
 						<ArrowDownIcon size={14} />
 						{product.discountPercentage}%
 					</Badge>
 				)}
 			</div>
 
+
 			<div className="flex flex-col gap-1">
 				<p className="overFlow-hiddem text-ellipsis whitespace-nowrap text-sm">
 					{product.name}
 				</p>
 
-				<div className="flex-items-center gap-2">
+				<div className="flex items-center gap-2">
 					{product.discountPercentage > 0 ? (
 						<>
 							<p className="font-semibold">
