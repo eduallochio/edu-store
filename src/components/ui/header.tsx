d2651737d878a0ae499bec010b138d88d5c8a979
eduallochio.github.io <43136553+eduallochio@users.noreply.github.com>
2023-11-21 18:26:00 -0300

    Revert "feat: correção de sintaxe"
    This reverts commit 52e401a23529d5d7670bd9e194c11e8d45b4c4bb.

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -11,12 +11,11 @@ import {
 } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
-import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback } from "@radix-ui/react-avatar";
 import { AvatarImage } from "./avatar";
 import { Separator } from "./separator";
-import Link from "next/link";
 
 const Header = () => {
       const { status, data } = useSession();
@@ -93,26 +92,17 @@ const Header = () => {
                                           Ofertas
                                     </Button>
 
-                                    <SheetClose asChild>
-                                          <Link href="/catalog">
-                                                <Button
-                                                      variant="outline"
-                                                      className="w-full justify-start gap-2"
-                                                >
-                                                      <ListOrderedIcon size={16} />
-                                                      Catálogo
-                                                </Button>
-                                          </Link>
-                                    </SheetClose>
+                                    <Button variant="outline" className="w-full justify-start gap-2 ">
+                                          <ListOrderedIcon size={16} />
+                                          Catálogo
+                                    </Button>
                               </div>
                         </SheetContent>
                   </Sheet>
 
-                  <Link href='/'>
-                        <h1 className="texte-lg font-semibold">
-                              <span className=" text-primary">MaBiDu</span> Store
-                        </h1>
-                  </Link>
+                  <h1 className="texte-lg font-semibold">
+                        <span className=" text-primary">MaBiDu</span> Store
+                  </h1>
 
                   <Button size="icon" variant={"outline"}>
                         <ShoppingCartIcon />
